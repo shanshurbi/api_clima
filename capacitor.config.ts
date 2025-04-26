0e254189b701152b72d03293a506d82e969d85cf
shanshurbi <rod.urbina@duocuc.cl>
2025-04-26 15:37:34 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'api_clima',
+  webDir: 'www'
+};
+
+export default config;
